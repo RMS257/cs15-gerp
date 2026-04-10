690456b6727b6b91fe4552e921d61449a2fd756a
Romil Shah <62564309+RMS257@users.noreply.github.com>
2026-04-10 14:18:47 -0400

    stripNonAlpha

---
+++ processing.cpp
@@ -0,0 +1,29 @@
+#include <cctype>
+#include <string>
+
+using namespace std;
+
+string stripNonAlphaNum(string s)
+{
+    int start = 0;
+    // end starts at the last character of the string
+    int end = s.length() - 1;
+
+    // move start forward until we hit an alphanumeric character
+    while (start <= end and not isalnum(s[start])) {
+        start++;
+    }
+
+    // move end backward until we hit an alphanumeric character
+    while (end >= start and not isalnum(s[end])) {
+        end--;
+    }
+
+    // if nothing valid remains, return empty string
+    if (start > end) {
+        return "";
+    }
+
+    // return the cleaned substring
+    return s.substr(start, end - start + 1);
+}
